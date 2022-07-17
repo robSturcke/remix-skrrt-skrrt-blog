96031ed3adbad60fdf22306767d3f2837f9abdcf
robSturcke <rob@rslabs.dev>
2022-07-16 21:24:28 -0400

    shows text for creating on slow connections

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,5 +1,6 @@
-import { Form, useActionData } from "@remix-run/react";
-import { ActionFunction, json } from "@remix-run/server-runtime";
+import { Form, useActionData, useTransition } from "@remix-run/react";
+import type { ActionFunction } from "@remix-run/server-runtime";
+import { json } from "@remix-run/server-runtime";
 import { redirect } from "@remix-run/server-runtime";
 import invariant from "tiny-invariant";
 import { createPost } from "~/models/post.server";
@@ -51,6 +52,10 @@ const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
 
 export default function NewPostRoute() {
   const errors = useActionData() as ActionData;
+
+  const transition = useTransition();
+  const isCreating = Boolean(transition.submission);
+
   return (
     <Form method="post">
       <p>
@@ -86,9 +91,10 @@ export default function NewPostRoute() {
       <p className="text-right">
         <button
           type="submit"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600"
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 disabled:bg-blue-300"
+          disabled={isCreating}
         >
-          Create Post
+          {isCreating ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
