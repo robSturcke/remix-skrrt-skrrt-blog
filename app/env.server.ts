f4d01e93da12085344f5092fab880437ba71d591
robSturcke <rob@rslabs.dev>
2022-07-30 12:39:04 -0400

    env.server fix

--- app/env.server.ts
+++ app/env.server.ts
@@ -1,13 +1,14 @@
-import invariant from 'tiny-invariant';
+import invariant from "tiny-invariant";
 
 export function getEnv() {
-  invariant(process.env.ADMIN_EMAIL, 'ADMIN_EMAIL should be defined')
+  invariant(process.env.ADMIN_EMAIL, "ADMIN_EMAIL should be defined");
+
   return {
     ADMIN_EMAIL: process.env.ADMIN_EMAIL,
-  }
+  };
 }
 
-type ENV = ReturnType<typeof getEnv>
+type ENV = ReturnType<typeof getEnv>;
 
 declare global {
   var ENV: ENV;
